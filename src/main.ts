c771a416398a3bef35938247d1cffb2f6aa8159c
Andrew Joll <andrewjoll@gmail.com>
2024-08-03 20:41:08 +1200

    wrap top level await

--- src/main.ts
+++ src/main.ts
@@ -19,119 +19,123 @@ const viewport = new Viewport(app.stage, 3000, 2000);
 const mouse = new Mouse(viewport);
 
 // Intialize the application.
-await app.init({
-  background: "#5E6F42",
-  resizeTo: window,
-  resolution: window.devicePixelRatio,
-});
+const init = async () => {
+  await app.init({
+    background: "#5E6F42",
+    resizeTo: window,
+    resolution: window.devicePixelRatio,
+  });
 
-const engine = Engine.create({
-  gravity: {
-    x: 0,
-    y: 0,
-  },
-});
+  const engine = Engine.create({
+    gravity: {
+      x: 0,
+      y: 0,
+    },
+  });
 
-const entityManager = new EntityManager(engine);
+  const entityManager = new EntityManager(engine);
 
-const wallWidth = 40;
+  const wallWidth = 40;
 
-const leftWall = Bodies.rectangle(
-  -(wallWidth * 0.5),
-  viewport.worldHeight * 0.5,
-  wallWidth,
-  viewport.worldHeight,
-  {
-    isStatic: true,
-  }
-);
-
-const rightWall = Bodies.rectangle(
-  viewport.worldWidth + wallWidth * 0.5,
-  viewport.worldHeight * 0.5,
-  wallWidth,
-  viewport.worldHeight,
-  {
-    isStatic: true,
-  }
-);
-
-const topWall = Bodies.rectangle(
-  viewport.worldWidth * 0.5,
-  -(wallWidth * 0.5),
-  viewport.worldWidth,
-  wallWidth,
-  {
-    isStatic: true,
-  }
-);
-
-const bottomWall = Bodies.rectangle(
-  viewport.worldWidth * 0.5,
-  viewport.worldHeight + wallWidth * 0.5,
-  viewport.worldWidth,
-  wallWidth,
-  {
-    isStatic: true,
-  }
-);
-
-// add all of the bodies to the world
-Composite.add(engine.world, [leftWall, rightWall, topWall, bottomWall]);
-
-Assets.addBundle("soldier", Soldier.assetBundle());
-Assets.addBundle("mouse", Mouse.assetBundle());
-Assets.addBundle("barrel", Barrel.assetBundle());
-
-await Assets.loadBundle("mouse");
-await Assets.loadBundle("soldier");
-await Assets.loadBundle("barrel");
-
-document.body.appendChild(app.canvas);
-
-const layerGround = new Container();
-const layerDebug = new Container();
-
-initDebugGraphics(layerDebug);
-
-const soldier = new Soldier(
-  viewport.worldWidth * 0.5,
-  viewport.worldHeight * 0.5
-);
-const barrel = new Barrel(700, 700);
-
-layerGround.addChild(mouse);
-
-entityManager.addEntity(soldier);
-entityManager.addEntity(barrel);
-
-// const emitter = new Emitter(soldier);
-// emitter.position.set(soldier.position.x, soldier.position.y);
-
-app.stage.addChild(layerGround, entityManager.container, layerDebug);
-
-for (let i = 0; i < 5; i++) {
-  const enemy = new Enemy(
-    900 - i * 50,
-    viewport.worldHeight * 0.5 - 200 + i * 100
+  const leftWall = Bodies.rectangle(
+    -(wallWidth * 0.5),
+    viewport.worldHeight * 0.5,
+    wallWidth,
+    viewport.worldHeight,
+    {
+      isStatic: true,
+    }
   );
 
-  entityManager.addEntity(enemy);
-}
+  const rightWall = Bodies.rectangle(
+    viewport.worldWidth + wallWidth * 0.5,
+    viewport.worldHeight * 0.5,
+    wallWidth,
+    viewport.worldHeight,
+    {
+      isStatic: true,
+    }
+  );
 
-const gameTime = new GameTime();
+  const topWall = Bodies.rectangle(
+    viewport.worldWidth * 0.5,
+    -(wallWidth * 0.5),
+    viewport.worldWidth,
+    wallWidth,
+    {
+      isStatic: true,
+    }
+  );
 
-gameTime.onTick((time) => {
-  Engine.update(engine, time.deltaMs);
+  const bottomWall = Bodies.rectangle(
+    viewport.worldWidth * 0.5,
+    viewport.worldHeight + wallWidth * 0.5,
+    viewport.worldWidth,
+    wallWidth,
+    {
+      isStatic: true,
+    }
+  );
 
-  entityManager.update(time, mouse);
+  // add all of the bodies to the world
+  Composite.add(engine.world, [leftWall, rightWall, topWall, bottomWall]);
 
-  // emitter.update(time, mouse);
+  Assets.addBundle("soldier", Soldier.assetBundle());
+  Assets.addBundle("mouse", Mouse.assetBundle());
+  Assets.addBundle("barrel", Barrel.assetBundle());
 
-  viewport.update(time, mouse);
-  mouse.update();
-});
+  await Assets.loadBundle("mouse");
+  await Assets.loadBundle("soldier");
+  await Assets.loadBundle("barrel");
 
-window.addEventListener("click", () => {
-  soldier.moveTo(mouse.worldPosition);
-});
+  document.body.appendChild(app.canvas);
+
+  const layerGround = new Container();
+  const layerDebug = new Container();
+
+  initDebugGraphics(layerDebug);
+
+  const soldier = new Soldier(
+    viewport.worldWidth * 0.5,
+    viewport.worldHeight * 0.5
+  );
+  const barrel = new Barrel(700, 700);
+
+  layerGround.addChild(mouse);
+
+  entityManager.addEntity(soldier);
+  entityManager.addEntity(barrel);
+
+  // const emitter = new Emitter(soldier);
+  // emitter.position.set(soldier.position.x, soldier.position.y);
+
+  app.stage.addChild(layerGround, entityManager.container, layerDebug);
+
+  for (let i = 0; i < 5; i++) {
+    const enemy = new Enemy(
+      900 - i * 50,
+      viewport.worldHeight * 0.5 - 200 + i * 100
+    );
+
+    entityManager.addEntity(enemy);
+  }
+
+  const gameTime = new GameTime();
+
+  gameTime.onTick((time) => {
+    Engine.update(engine, time.deltaMs);
+
+    entityManager.update(time, mouse);
+
+    // emitter.update(time, mouse);
+
+    viewport.update(time, mouse);
+    mouse.update();
+  });
+
+  window.addEventListener("click", () => {
+    soldier.moveTo(mouse.worldPosition);
+  });
+};
+
+init();
